f494f15e53fe5d829d4cf64facce2006c8993a8b
Kaiseriscool <86335834+Kaiseriscool@users.noreply.github.com>
2022-10-13 22:15:49 -0400

    Update sv_otherfunc.lua

--- klib/klib_main/funcs/sv_otherfunc.lua
+++ klib/klib_main/funcs/sv_otherfunc.lua
@@ -1,11 +1,1 @@
-function Klib:ChatPrint(pl, msg , chat_tag)
-    local CT == chat_tag or ""
-    pl:ChatPrint("" .. CT .. " " .. msg .. "")
-end
-
-function Klib:ChatAll(msg , chat_tag)
-    local CT == chat_tag or ""
-    for k , pl in pairs(player.GetAll()) do
-    pl:ChatPrint("" .. CT .. " " .. msg .. "")
-    end
-end
+print("a")
